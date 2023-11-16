5536e96d27c6241ef9e2a7e89d1f86d146be772f
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-11-16 18:19:58 +0000

    Finished-HTML/CSS/JS-functionallity/resposiveness-layout

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const $entriesList = document.querySelector('.unordered');
 const $anchorEntries = document.querySelector('.entriesBtn');
 const $entryForm = document.querySelector('.query_one');
 const $entriesBottom = document.querySelector('.query_two');
+const $newBtn = document.querySelector('.new_js');
 
 $photoId.addEventListener('input', (e) => {
   $imgSrcChange.setAttribute('src', e.target.value);
@@ -13,13 +14,13 @@ $photoId.addEventListener('input', (e) => {
 // submit event
 
 $form.addEventListener('submit', (e) => {
+  e.preventDefault();
   const objStorageFormInputs = {
     entryId: data.nextEntryId,
     title: e.target.elements.title.value,
     photoID: e.target.elements.photoID.value,
     textarea: e.target.elements.textarea.value,
   };
-  e.preventDefault();
 
   data.nextEntryId++;
 
@@ -31,9 +32,9 @@ $form.addEventListener('submit', (e) => {
 
   // render the renderEntry callback on the form if the form's submitted
 
-  const callRender = renderEntry(objStorageFormInputs);
+  const $callRender = renderEntry(objStorageFormInputs);
 
-  $entriesList.prepend(callRender);
+  $entriesList.prepend($callRender);
   viewSwap('entries');
   toggleEntries();
 });
@@ -43,48 +44,48 @@ $form.addEventListener('submit', (e) => {
 const renderEntry = (entry) => {
   // create the li element dom tree
 
-  const liCreation = document.createElement('li');
-  liCreation.className = 'row';
+  const $liCreation = document.createElement('li');
+  $liCreation.className = 'row';
 
   // div element creation
 
-  const div = document.createElement('div');
-  div.className = 'column-half';
+  const $div = document.createElement('div');
+  $div.className = 'column-half';
   // div 2 creation
 
-  const div2 = document.createElement('div');
-  div2.className = 'column-half';
+  const $div2 = document.createElement('div');
+  $div2.className = 'column-half';
 
   // img element creation
 
-  const imgDomTree = document.createElement('img');
-  imgDomTree.setAttribute('src', entry.photoID);
-  imgDomTree.setAttribute('alt', 'img_from_Dom');
+  const $imgDomTree = document.createElement('img');
+  $imgDomTree.setAttribute('src', entry.photoID);
+  $imgDomTree.setAttribute('alt', 'img_from_Dom');
 
   // p one element creation
-  const h1 = document.createElement('p');
-  h1.className = 'bold appnedHtwo';
-  h1.textContent = entry.title;
+  const $h1 = document.createElement('p');
+  $h1.className = 'bold appnedHtwo';
+  $h1.textContent = entry.title;
 
   // p two element creation
-  const p2 = document.createElement('p');
-  p2.className = 'appendChildFromJs';
-  p2.textContent = entry.textarea;
+  const $p2 = document.createElement('p');
+  $p2.className = 'appendChildFromJs';
+  $p2.textContent = entry.textarea;
   // appending to the DOM with appendChild
 
-  liCreation.appendChild(div);
-  liCreation.appendChild(div2);
-  div.appendChild(imgDomTree);
-  div2.appendChild(h1);
-  div2.appendChild(p2);
+  $liCreation.appendChild($div);
+  $liCreation.appendChild($div2);
+  $div.appendChild($imgDomTree);
+  $div2.appendChild($h1);
+  $div2.appendChild($p2);
 
   //  returrns the li element with all the dom nodes/creation
 
-  return liCreation;
+  return $liCreation;
 };
 
 // Calling the addEventListner on the DOM'S document
-const nullMsg = document.querySelector('.nullMsg');
+const $nullMsg = document.querySelector('.nullMsg');
 
 document.addEventListener('DOMContentLoaded', () => {
   for (let i = 0; i < data.entries.length; i += 1) {
@@ -98,9 +99,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function toggleEntries() {
   if (data.entries.length === 0) {
-    nullMsg.classList.add('block');
+    $nullMsg.classList.add('block');
   } else if (data.entries.length !== 0) {
-    nullMsg.classList.add('hidden');
+    $nullMsg.classList.add('hidden');
   }
 }
 // Creating the viewSwap function
@@ -116,6 +117,12 @@ function viewSwap(entries) {
     $entriesBottom.classList.add('hidden');
   }
 }
+
+// working with the form's new button
+
+$newBtn.addEventListener('click', () => {
+  viewSwap('entry-form');
+});
 
 // working with the entries anchor tag on top and calling an event on it
 
